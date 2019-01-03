f6eaace0a675d432f83f4d4dab3a58acf083146a
Dmitry Logvinovich <adlerstone@yahoo.co.uk>
2019-01-03 20:42:28 +0100

    adding POJOs. Closes #2.

---
+++ src/main/java/ch/zhaw/logvidmi/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package ch.zhaw.logvidmi.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/ch/zhaw/logvidmi/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package ch.zhaw.logvidmi.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
